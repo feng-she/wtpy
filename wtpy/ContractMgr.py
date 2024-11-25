0f54d4cff952296a1b8aa147f2246904e53e7f6c
feng-she <54500831+feng-she@users.noreply.github.com>
2024-11-25 10:38:25 +0800

    股指期权标准合约解析后的结果跟其官网所叙述不一致，同时按照现在的解析方式，很多stdCode参数的函数都调用失败，我们进行修改，返回官网所标注的stdCode的形式

--- wtpy/ContractMgr.py
+++ wtpy/ContractMgr.py
@@ -71,9 +71,11 @@ class ContractMgr:
                     cInfo.shortMarginRatio = float(cObj["shortmarginratio"])
 
                 if "product" in cObj:
-                    cInfo.product = cObj["product"]                    
-                    #股票标准代码为SSE.000001，期货标准代码为SHFE.rb.2010
-                    if cInfo.code[:len(cInfo.product)] == cInfo.product:
+                    cInfo.product = cObj["product"]
+                    #股票标准代码为SSE.000001，期货标准代码为SHFE.rb.2010,股指期权遵循商品期权的方式，所以stdcode不包含product
+                    if cInfo.code[:len(cInfo.product)] == cInfo.product and cInfo.product in ['HO', 'MO', 'HO']:
+                        cInfo.stdCode = exchg + "." + cInfo.code
+                    elif cInfo.code[:len(cInfo.product)] == cInfo.product:
                         month = cInfo.code[len(cInfo.product):]
                         if len(month) < 4:
                             month = "2" + month
